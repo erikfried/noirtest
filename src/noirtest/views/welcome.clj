5770674adf5e5e4eb9f11bebc86648a967c3cb61
Erik Fried <erik.fried@aftonbladet.se>
2012-08-29 07:22:32 +0200

    initial project structure

---
+++ src/noirtest/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns noirtest.views.welcome
+  (:require [noirtest.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to noirtest"]))
